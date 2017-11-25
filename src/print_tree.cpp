e875fd40f8030ff09fbbf60d620ec5bc129b8e7d
Sarandia <sarandia@sarandias-mbp.lan>
2017-11-25 15:44:05 -0500

    trying to compile

--- src/print_tree.cpp
+++ src/print_tree.cpp
@@ -2,12 +2,14 @@
 #include "tree.h"
 
 using namespace lock_free_rbtree;
-
-void print_tree(RBTree<int, int>* t) {
-
-}
+using namespace std;
 
 int main() {
-  print_tree(new RBTree<int, int>());
-  return 0;
+	RBTree<int, int> *t = new RBTree<int, int>();
+  	
+	cout << "Successfully created rb tree object" << endl;
+	t->Insert(5,5);
+	cout << "Inserted 5" << endl;
+	t->Remove(5);
+  	return 0;
 }
