8d4dd15988f57ca27afb3fb58d5c1d5c778b533b
Sarandia <sarandia@sarandias-mbp.lan>
2017-12-03 15:24:36 -0500

    bench_modify

--- src/print_tree.cpp
+++ src/print_tree.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include "tree.h"
 #include "treeprinter.h"
+#include <ctime>
 using namespace lock_free_rbtree;
 using namespace std;
 
 int main() {
 	RBTree<int, int> *t = new RBTree<int, int>();
-	int start = 1, finish = 10;
+	int start = 1, finish = 200000;
   	
 	cout << "Successfully created rb tree object" << endl;
+	clock_t code_start, code_end;
+    code_start = clock();
+
 	for (int i = start; i <= finish; i++) {
 		t->Insert(i,i);
 		//t->print_tree();
@@ -19,6 +23,10 @@ int main() {
 			exit(1);
 		}*/
 	}
+	code_end = clock();
+    double msecs = ((double) (code_end - code_start)) * 1000 / CLOCKS_PER_SEC;
+    cout << "Execution took " << msecs << " miliseconds." << endl;
+
 	//print2D(t->GetRoot());
 	for (int i = start; i <= finish; i++) {
 		TreeNode<int, int> *found = t->Search(i);
